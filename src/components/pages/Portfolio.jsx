3a6ae97f5c7f47c518ea61e3f02287c78784c5f1
J_D_Nye <garwoodjdm@gmail.com>
2023-10-08 17:02:57 +1100

    project links added

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -4,26 +4,50 @@ export default function Portfolio() {
             <h1></h1>
             <div className="row">
                 <div id="project_section" className="col">
-                    <div id="project_link"></div>
+                        <a href="https://enigmatic-badlands-67333-655bf991c860.herokuapp.com/">
+                            <div id="project_link">
+                                Tech Blog
+                            </div>
+                        </a>
                 </div>
                 <div id="project_section" className="col">
-                    <div id="project_link"></div>
+                    <a href="https://powerful-sierra-12005-787af3607f3d.herokuapp.com/">
+                            <div id="project_link">
+                                RecipeBox
+                            </div>
+                    </a>
                 </div>
             </div>
             <div className="row">
                 <div id="project_section" className="col">
-                    <div id="project_link"></div>
+                    <a href="https://j-d-garwood.github.io/Marvel-Character-Browser/">
+                        <div id="project_link">
+                                Marvel Browser
+                        </div>
+                    </a>
                 </div>
                 <div id="project_section" className="col">
-                    <div id="project_link"></div>   
+                    <a href="https://j-d-garwood.github.io/Weather-Dashboard/">
+                            <div id="project_link">
+                                Weather Dashboard
+                            </div>
+                    </a>
                 </div>
             </div>
             <div className="row">
                 <div id="project_section" className="col">
-                    <div id="project_link"></div>
+                        <a href="https://gentle-forest-87600-982310ed4a31.herokuapp.com/">
+                            <div id="project_link">
+                                NoteTaker
+                            </div>
+                        </a>
                 </div>
                 <div id="project_section" className="col">
-                    <div id="project_link"></div>
+                        <a href="https://j-d-garwood.github.io/04-Web-Dev_Quiz/">
+                            <div id="project_link">
+                                Web Dev Quiz
+                            </div>
+                        </a>
                 </div>
             </div>
         </div>
