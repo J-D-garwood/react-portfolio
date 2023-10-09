26e5421a4b80e93ed411c62052f8b2a5a804655e
J_D_Nye <garwoodjdm@gmail.com>
2023-10-09 22:01:46 +1100

    footer links finalised

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,12 +9,12 @@ function Footer() {
                         </a>
                     </div>
                     <div id="foot-col" className="col-6 col-md-3 col-lg-2 text-center align-items-center">
-                        <a id="foot-link" href="#">
+                        <a id="foot-link" href="https://www.linkedin.com/in/james-g-16412624b/">
                             <img id="foot-link-svg" src="..\..\assets\LinkedIn_icon_circle.svg" height="50" width="50"></img>
                         </a>
                     </div>
                     <div id="foot-col" className="col-6 col-md-3 col-lg-2 text-center align-items-center">
-                        <a id="foot-link" href="#">
+                        <a id="foot-link" href="https://j-d-garwood.github.io/ComingSoon/">
                             <img id="foot-link-svg" src="..\..\assets\OFIcon.svg" height="50" width="50"></img>
                         </a>
                     </div>
