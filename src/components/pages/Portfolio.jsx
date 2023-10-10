d9ae385b8bb7c930ade91e8c1c85321d61f62afa
J_D_Nye <garwoodjdm@gmail.com>
2023-10-10 23:58:32 +1100

    all sections contents finished

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -30,15 +30,16 @@ export default function Portfolio() {
             link: "https://j-d-garwood.github.io/04-Web-Dev_Quiz/"
         }
     ]
+    console.log(myProjects[0])
     return (
         <div className="container-fluid">
             <div className='row justify-content-center'>
-                <Project projects={myProjects[0]}/>
-                <Project projects={myProjects[1]}/>
-                <Project projects={myProjects[2]}/>
-                <Project projects={myProjects[3]}/>
-                <Project projects={myProjects[4]}/>
-                <Project projects={myProjects[5]}/>
+                <Project title={myProjects[0].title} link={myProjects[0].link}/>
+                <Project title={myProjects[1].title} link={myProjects[1].link}/>
+                <Project title={myProjects[2].title} link={myProjects[2].link}/>
+                <Project title={myProjects[3].title} link={myProjects[3].link}/>
+                <Project title={myProjects[4].title} link={myProjects[4].link}/>
+                <Project title={myProjects[5].title} link={myProjects[5].link}/>
             </div>
         </div>
     )
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,7 +3,21 @@ import { validateEmail } from '../../utils/helpers';
 
 export default function Resume() {
     return (
-        <div>resume</div>
+        <div className='row justify-content-center'>
+            <div className='col-12 col-md-8 col-lg-6'>
+                <h2>Some of my skills are..</h2>
+                <ul>
+                    <li>Proficient in Front-End Development</li>
+                    <li>React.js Expertise</li>
+                    <li>Git Version Control</li>
+                    <li>Cross-Browser Compatibility</li>
+                    <li>API Integration</li>
+                    <li>Mobile-First Design</li>
+                    <li>Effective Communication</li>
+                </ul>
+                <a href="..\..\..\assets\resume.pdf">Download my resume</a>
+            </div>
+        </div>
     );
 }
 
